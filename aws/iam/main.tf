1bce6742301e1e6562b1c03c8954788386a332ac
jp-taku2 <jp-taku2@users.noreply.github.com>
2021-07-03 21:22:14 +0900

    refactor (#2)

--- aws/iam/main.tf
+++ aws/iam/main.tf
@@ -11,35 +11,34 @@ resource "aws_iam_user" "jp_taku2" {
   }
 }
 
+data "aws_iam_policy_document" "sandbox-powe-user" {
+  statement{
+    sid = "AllowSandboxPoweUser"
+    effect = "Allow"
+    actions = [
+      "iam:CreateServiceLinkedRole",
+      "iam:DeleteServiceLinkedRole",
+      "iam:ListRoles",
+      "organizations:DescribeOrganization",
+      "account:ListRegions"
+    ]
+    resources = [
+      "*"
+    ]
+  },
+  statement {
+    sid = "SandboxPoweUserNotAction"
+    effect = "Allow"
+    not_actions = [
+      "iam:*",
+      "organizations:*",
+      "account:*"
+    ]
+  },
+}
+
 resource "aws_iam_user_policy" "power-user" {
   name = "power-user"
   user = aws_iam_user.jp_taku2.name
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "iam:CreateServiceLinkedRole",
-        "iam:DeleteServiceLinkedRole",
-        "iam:ListRoles",
-        "organizations:DescribeOrganization",
-        "account:ListRegions"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    },
-    {
-      "NotAction": [
-        "iam:*",
-        "organizations:*",
-        "account:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+  policy = data.aws_iam_policy_document.sandbox-powe-user.json
 }
